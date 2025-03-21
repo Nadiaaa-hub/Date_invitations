e72290d743dd72ea5b01eb68a07efa4f3e554963
Nadia <nadya2000006@gmail.com>
2025-03-21 13:12:04 +0200

    Опис змін

---
+++ script.js
@@ -0,0 +1,206 @@
+const inputContainer = document.querySelector(".input-container");
+const secondWindow = document.querySelector(".second-window");
+const thirdWindow = document.querySelector(".third-window");
+const fourthWindow = document.querySelector(".fourth-window");
+const fifthWindow = document.querySelector(".fifth-window");
+const sixWindow = document.querySelector(".six-window");
+
+const yesButton = document.querySelector(".yes-button");
+const noButton = document.querySelector(".no-button");
+const maybeButton = document.querySelector(".maybe-button");
+const button = document.querySelector(".button");
+const nextButton = document.querySelectorAll(".next-button");
+const cancelButtons = document.querySelectorAll(".cancel-button");
+const fourthCancelButton = document.querySelector(".fourth-cancel-button");
+
+const thirdNextButton = document.querySelector(".third-window .next-button");
+const fourthNextButton = document.querySelector(".fourth-window .next-button");
+const fifthNextButton = document.querySelector(".fifth-window .next-button");
+
+const imageElement = document.createElement("img");
+const imageContainers = document.querySelectorAll(".image-container");
+const chosenDateType = document.getElementById("chosen-date-type");
+const chosenTimeType = document.getElementById("chosen-time-type");
+
+const inputThird = document.querySelector(".input-third");
+const inputField = document.querySelector(".fourth-window input");
+const inputFifth = document.querySelector(".input-fifth");
+
+imageElement.src = "./img/jpg";
+imageElement.autoplay = true;
+imageElement.muted = true;
+imageElement.loop = true;
+imageElement.classList.add("hidden");
+document.body.appendChild(imageElement);
+
+function checkInput() {
+  const input = document.querySelector(".input").value;
+  if (input === "") {
+    alert("Please fill in the field.");
+  } else if (input === "Yura") {
+    inputContainer.classList.add("hidden");
+    thirdWindow.classList.add("hidden");
+    secondWindow.classList.remove("hidden");
+  } else {
+    alert("You are not my date, sorry((");
+  }
+}
+button.addEventListener("click", checkInput);
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//     checkInput();
+//     button.classList.add("button-enter-pressed");
+//     setTimeout(() => {
+//       button.classList.remove("button-enter-pressed");
+//     }, 400);
+//   }
+// });
+
+if (yesButton) {
+  yesButton.addEventListener("click", () => {
+    secondWindow.classList.add("hidden");
+    thirdWindow.classList.remove("hidden");
+  });
+}
+
+if (noButton) {
+  noButton.addEventListener("click", () => {
+    imageElement.classList.remove("hidden");
+    imageElement.classList.add("full-screen");
+
+    setTimeout(() => {
+      imageElement.classList.add("visible");
+    }, 40);
+
+    setTimeout(() => {
+      imageElement.classList.remove("visible");
+      imageElement.classList.add("hidden");
+    }, 3000);
+
+    secondWindow.classList.add("hidden");
+    inputContainer.classList.remove("hidden");
+  });
+}
+
+if (maybeButton) {
+  maybeButton.addEventListener("click", () => {
+    // let currentImageIndex = 0;
+
+    // function showImage() {
+    //   if (currentImageIndex < imageContainers.length) {
+    //     const currentImage = imageContainers[currentImageIndex];
+    //     currentImage.classList.remove("hidden");
+    //     currentImage.classList.add("visible");
+
+    //     currentImage.classList.remove("visible");
+    //     currentImage.classList.add("hidden");
+
+    //     currentImageIndex++;
+
+    //     if (currentImageIndex < imageContainers.length) {
+    //       showImage();
+    //     }
+    //   }
+    // }
+    alert("Incorrect answer");
+    secondWindow.classList.add("hidden");
+    inputContainer.classList.remove("hidden");
+  });
+}
+
+if (fourthCancelButton) {
+  fourthCancelButton.addEventListener("click", () => {
+    inputField.value = "";
+
+    inputField.focus();
+    secondWindow.classList.add("hidden");
+  });
+}
+
+cancelButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    inputThird.selectedIndex = 0;
+    inputFifth.selectedIndex = 0;
+  });
+});
+
+if (thirdNextButton) {
+  thirdNextButton.addEventListener("click", () => {
+    const selectedOption = document.querySelector(".input-third").value;
+
+    if (!selectedOption) {
+      alert("Please choose a date type!");
+      return;
+    }
+
+    let dateTypeText = "";
+    switch (selectedOption) {
+      case "1":
+        dateTypeText = "romantic dinner?";
+        break;
+      case "2":
+        dateTypeText = "outdoor adventure?";
+        break;
+      case "3":
+        dateTypeText = "chill hangout?";
+        break;
+      case "4":
+        dateTypeText = "surprise me!";
+        break;
+    }
+
+    document.getElementById("chosen-date-type").textContent = dateTypeText;
+    document.querySelector(".third-window").classList.add("hidden");
+    document.querySelector(".fourth-window").classList.remove("hidden");
+  });
+}
+
+if (fourthNextButton) {
+  fourthNextButton.addEventListener("click", () => {
+    const selectedOptionPlace = document.querySelector(".input-fourth").value;
+    if (!selectedOptionPlace) {
+      alert("Please enter a place!");
+      return;
+    }
+    console.log("Selected place:", selectedOptionPlace);
+
+    document.getElementById("chosen-place-type").textContent =
+      selectedOptionPlace;
+
+    document.querySelector(".fourth-window").classList.add("hidden");
+    document.querySelector(".fifth-window").classList.remove("hidden");
+  });
+
+  if (fifthNextButton) {
+    fifthNextButton.addEventListener("click", () => {
+      const selectedOptionTime = document.querySelector(".input-fifth").value;
+      const selectedOptionPlace = document.querySelector(".input-fourth").value;
+
+      if (!selectedOptionTime || !selectedOptionPlace) {
+        alert("Please choose a time!");
+        return;
+      }
+
+      let timeTypeText = "";
+      switch (selectedOptionTime) {
+        case "1":
+          timeTypeText = "Morning";
+          break;
+        case "2":
+          timeTypeText = "Afternoon";
+          break;
+        case "3":
+          timeTypeText = "Evening";
+          break;
+      }
+
+      document.getElementById("chosen-place-type").textContent =
+        selectedOptionPlace;
+      document.getElementById("chosen-time-type").textContent = timeTypeText;
+
+      document.querySelector(".fifth-window").classList.add("hidden");
+      document.querySelector(".six-window").classList.remove("hidden");
+    });
+  }
+}
